501604f7a66c402df76abf7b584b0c9990e49fae
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2025-12-22 19:59:21 -0500

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Antony Huaman Alikhan on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
